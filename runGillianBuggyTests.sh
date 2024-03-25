32c331ea57d45a68804e50785e96e1b39affc034
Nat Karmios <nat@karmios.com>
2024-03-25 16:02:03 +0000

    Reorganise tests

--- runGillianBuggyTests.sh
+++ runGillianBuggyTests.sh
@@ -4,11 +4,11 @@ set -e
 
 ./clean.sh
 
-TESTS_DIR="for-gillian/bugs"
-LIB_HEADERS_DIR="lib-with-bugs/include"
-LIB_SRC_DIR="lib-with-bugs"
-UTILS_HEADERS_DIR="for-gillian/test-utils"
-UTILS_SRC_DIR="for-gillian/test-utils"
+TESTS_DIR="gillian/bugs"
+LIB_HEADERS_DIR="libs/bugged/include"
+LIB_SRC_DIR="libs/bugged"
+UTILS_HEADERS_DIR="gillian/utils"
+UTILS_SRC_DIR="gillian/utils"
 
 echo "Running buggy tests using Gillian-C..."
 gillian-c bulk-wpst ${TESTS_DIR} -I ${LIB_HEADERS_DIR} -I ${UTILS_HEADERS_DIR} \
--- runGillianTests.sh
+++ runGillianTests.sh
@@ -4,11 +4,11 @@ set -e
 
 ./clean.sh
 
-TESTS_DIR="for-gillian/normal"
-LIB_HEADERS_DIR="lib/include"
-LIB_SRC_DIR="lib"
-UTILS_HEADERS_DIR="for-gillian/test-utils"
-UTILS_SRC_DIR="for-gillian/test-utils"
+TESTS_DIR="gillian/normal"
+LIB_HEADERS_DIR="libs/fixed/include"
+LIB_SRC_DIR="libs/fixed"
+UTILS_HEADERS_DIR="gillian/utils"
+UTILS_SRC_DIR="gillian/utils"
 
 echo "Running tests using Gillian-C..."
 gillian-c bulk-wpst ${TESTS_DIR} -I ${LIB_HEADERS_DIR} -I ${UTILS_HEADERS_DIR} \
